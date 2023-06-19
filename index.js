cd87ecd49742990b5a5321cce07f62eab2554b7a
YogeshSahu0 <yogesh.sahu@opensenselabs.com>
2023-06-19 16:42:23 +0530

    first commit!

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ const git = simpleGit({
   baseDir: "./",
 });
 
-git
-  .add(".")
-  .then(() => {
-    console.log("Files staged for commit.");
-  })
-  .catch((err) => {
-    console.error("Error staging files:", err);
-  });
+// git
+//   .add(".")
+//   .then(() => {
+//     console.log("Files staged for commit.");
+//   })
+//   .catch((err) => {
+//     console.error("Error staging files:", err);
+//   });
 
-git
-  .commit("Added new data")
-  .then(() => {
-    console.log("Changes committed.");
-  })
-  .catch((err) => {
-    console.error("Error committing changes:", err);
-  });
+// git
+//   .commit("adding data")
+//   .then(() => {
+//     console.log("Changes committed.");
+//   })
+//   .catch((err) => {
+//     console.error("Error committing changes:", err);
+//   });
 
-git.push('origin', 'main')
-  .then(() => {
-    console.log('Changes pushed to remote repository.');
-  })
-  .catch((err) => {
-    console.error('Error pushing changes:', err);
-  });
+// git.push('origin', 'main')
+//   .then(() => {
+//     console.log('Changes pushed to remote repository.');
+//   })
+//   .catch((err) => {
+//     console.error('Error pushing changes:', err);
+//   });
+
+simpleGit()
+   .add('./*')
+   .commit('first commit!')
+   .addRemote('origin', 'git@github.com:yogeshsahu15379/node-to-git.git')
+   .push(['-u', 'origin', 'master'], () => console.log('done'));
